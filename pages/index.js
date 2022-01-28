60bf1cdd8bd36d450a8cb43991917f3e0caee10e
jlokos <71952027+jlokos@users.noreply.github.com>
2022-01-28 12:04:08 -0500

    init tailwinds + nextjs

--- pages/index.js
+++ pages/index.js
@@ -12,8 +12,8 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+        <h1 className={"text-5xl"}>
+          Hey there!
         </h1>
 
         <p className={styles.description}>
